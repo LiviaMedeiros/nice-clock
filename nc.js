3b50dd2e54ea4705587f456a93ec5e78068cf870
LiviaMedeiros <livia@cirno.name>
2022-03-14 15:20:22 +0800

    init

---
+++ nc.js
@@ -0,0 +1,5 @@
+'use strict';
+((a,b) => b.addEventListener('DOMContentLoaded', () => ((n,o) => (o('ur',(o('kr',(o('nii',(
+  n.getMilliseconds()/1E3+n.getSeconds())/60)+n.getMinutes())/60)/5+n.getHours())/12),
+  a.forEach((ㅇ,ㅁ) => Object.assign(ㅁ,{transform:'rotate('+ㅇ+'turn)'}))))(new Date,
+/* is big */(ㅁ,ㅇ) => (a.set(b.getElementById(ㅁ).style,.5+ㅇ),ㅇ))))(new Map, document);
